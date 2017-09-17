46fbeffc209fa1be9f5bc5966c6c01d3ec4ee1c2
KiranTeegala <teegala.kiran123@gmail.com>
2017-09-17 18:37:36 -0500

    testing jenkins CD 6

--- src/main/java/com/controller/VendorController.java
+++ src/main/java/com/controller/VendorController.java
@@ -24,7 +24,7 @@ public class VendorController {
 	private String searchValue;
 	@Autowired
 	private  VendorService vendorService;
-	@RequestMapping(value="/getAllVendors")
+	@RequestMapping(value="/getAllVendordetails")
 	public @ResponseBody List<Vendor> findAll() {
 		List<Vendor> users=vendorService.findAllusers();
 		for( Vendor ven: users){
